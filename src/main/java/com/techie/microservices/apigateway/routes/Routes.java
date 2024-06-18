641d62d000aab426ff48a657cabd3eff07ad7388
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-06-18 17:37:03 +0700

    add routes for order and inventory service

--- src/main/java/com/techie/microservices/apigateway/routes/Routes.java
+++ src/main/java/com/techie/microservices/apigateway/routes/Routes.java
@@ -19,4 +19,20 @@ public class Routes {
                 .build();
     }
 
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceRoute() {
+        return GatewayRouterFunctions
+                .route("order_service")
+                .route(RequestPredicates.path("/api/order"), HandlerFunctions.http("http://localhost:8081"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceRoute() {
+        return GatewayRouterFunctions
+                .route("inventory_service")
+                .route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http("http://localhost:8082"))
+                .build();
+    }
+
 }
